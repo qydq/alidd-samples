ca861782f3801521938e5a20b4951f70203e1f0d
qydq <qyddai@163.com>
2019-12-31 19:44:47 +0800

    视频模块

--- xmediac/src/main/java/com/sunsty/xmediac/util/VideoPlayer.java
+++ xmediac/src/main/java/com/sunsty/xmediac/util/VideoPlayer.java
@@ -5,30 +5,39 @@ import android.view.Surface;
 public class VideoPlayer {
 
     //视频解码
-	public native static void decode(String input,String output);
-	//视频播放
-	public native void render(String input,Surface surface);
-	//音频解码
-    public native void audioDecode(String input,String output);
-	//音频播放
-	public native void audioPlayer(String input);
+    public native static void decode(String input, String output);
+
+    //视频播放
+    public native void render(String input, Surface surface);
+
+    //音频解码
+    public native void audioDecode(String input, String output);
+
+    //音频播放
+    public native void audioPlayer(String input);
+
     //音视频播放
-	public native void play(String input,Surface surface);
-	//视频转码压缩
-	public native void ffmpegCmdUtil(int argc,String[] argv);
-	//添加水印
-	public native void addWatermark(int argc,String[] argv);
-	static{
-		System.loadLibrary("avutil-54");
-		System.loadLibrary("swresample-1");
-		System.loadLibrary("avcodec-56");
-		System.loadLibrary("avformat-56");
-		System.loadLibrary("swscale-3");
-		System.loadLibrary("postproc-53");
-		System.loadLibrary("avfilter-5");
-		System.loadLibrary("avdevice-56");
-		System.loadLibrary("myffmpeg");
-	}
+    public native void play(String input, Surface surface);
 
+    //视频转码压缩
+    public native void ffmpegCmdUtil(int argc, String[] argv);
 
+    //添加水印
+    public native void addWatermark(int argc, String[] argv);
+
+    static {
+        try {
+            System.loadLibrary("avutil-54");
+            System.loadLibrary("swresample-1");
+            System.loadLibrary("avcodec-56");
+            System.loadLibrary("avformat-56");
+            System.loadLibrary("swscale-3");
+            System.loadLibrary("postproc-53");
+            System.loadLibrary("avfilter-5");
+            System.loadLibrary("avdevice-56");
+            System.loadLibrary("myffmpeg");
+        } catch (UnsatisfiedLinkError e) {
+            e.printStackTrace();
+        }
+    }
 }
--- xmediac/src/main/jni/Application.mk
+++ xmediac/src/main/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_MODULES := myffmpeg
-# armeabi-v7a ,armeabi arm64-v8a可选
-APP_ABI := arm64
+# armeabi-v7a ,armeabi 可选
+APP_ABI := arm64-v8a
 APP_PLATFORM := android-17
